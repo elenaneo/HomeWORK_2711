769a0b63691290cd8f09d3df9c4d3650cec173f0
elenaneo <57110448+elenaneo@users.noreply.github.com>
2019-12-08 16:39:35 +0500

    Update Tas3_2711.java

--- Tas3_2711.java
+++ Tas3_2711.java
@@ -1,13 +1,26 @@
 import java.util.Collection;
 
+import java.util.*;
+
 public class Tas3_2711 {
-    public static void main(Collection inCollection) {
+    public static void main(String[] args) {
+        String inStr = "A new year has begun "+
+                       "A new year for trying " +
+                       "for a year that is new";
+        String[] str=inStr.split(" ");
+
+        ArrayList<String> Test= new  ArrayList <> (Arrays.asList(str));
+        System.out.println(Test.toString());
+        System.out.println(removeDuplicates(Test).toString());
+       // Test.add()
 
 
     }
-    public Collection removeDuplicates (Collection inCollection)
+    public static Collection removeDuplicates (Collection inCollection)
     {
-
+        Set set = new HashSet(inCollection);
+        inCollection.clear();
+        inCollection.addAll(set);
         return inCollection;
     }
 }
